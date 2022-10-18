ff9a3b5c015dbf0f776bd56548380ed7f525de45
Chloe Law <chloelaw75@hotmail.com>
2022-10-18 11:53:48 -0400

    Update c.sql

--- Phase_1/3_Querying_MovieLens_Database/c.sql
+++ Phase_1/3_Querying_MovieLens_Database/c.sql
@@ -1,3 +1,5 @@
+-- Print the distinct genres in the database and their corresponding number of movies N where N is greater than 1000, sorted in the ascending order of N.
+
 SELECT genre, count as numberOfMovies
 FROM (
     SELECT genre, COUNT(movies.title) as count
