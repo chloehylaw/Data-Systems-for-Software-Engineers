ae38183c42241a46b2f0d7a87810780d1c8b6607
Chloe Law <chloelaw75@hotmail.com>
2022-10-18 11:52:56 -0400

    Update b.sql

--- Phase_1/3_Querying_MovieLens_Database/b.sql
+++ Phase_1/3_Querying_MovieLens_Database/b.sql
@@ -1,4 +1,4 @@
--- Print all movie titles starring ’Daniel Craig’, sorted in an ascending alphabetical order
+-- Print names of the cast of the movie ’The Dark Knight’ in an ascending alphabetical order.
 
 SELECT a.name
 FROM movies m, actors a
