c53cbdc799fc1bb517714077248ce4e6461d5f1d
Chloe Law <chloelaw75@hotmail.com>
2022-10-31 15:26:50 -0400

    Delete Phase_1/3_Querying_MovieLens_Database directory

--- Phase_1/3_Querying_MovieLens_Database/a.sql
+++
@@ -1,7 +0,0 @@
--- Print all movie titles starring ’Daniel Craig’, sorted in an ascending alphabetical order.
-
-SELECT movies.title
-FROM movies, actors
-WHERE actors.mid = movies.mid
-    AND actors.name = 'Daniel Craig'
-ORDER BY movies.title;
--- Phase_1/3_Querying_MovieLens_Database/b.sql
+++
@@ -1,7 +0,0 @@
--- Print names of the cast of the movie ’The Dark Knight’ in an ascending alphabetical order.
-
-SELECT a.name
-FROM movies m, actors a
-WHERE m.title = 'The Dark Knight'
-    AND m.mid = a.mid
-ORDER BY name;
